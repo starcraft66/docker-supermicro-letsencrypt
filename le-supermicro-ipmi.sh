cd1e5c35538ef6f30ed51b9e3bc929705dc48f81
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-12-23 23:04:37 -0500

    Temp remove `set -e` before calling openssl

--- le-supermicro-ipmi.sh
+++ le-supermicro-ipmi.sh
@@ -23,8 +23,10 @@ if [ -z ${LE_EMAIL+x} ]; then
 fi
 
 #Check if the certificate is expiring soon
+set +e
 echo | openssl s_client -servername $IPMI_DOMAIN -connect $IPMI_DOMAIN:443 2>/dev/null | openssl x509 -noout -checkend 2592000
 if [ "$?" == "1" ]; then
+set -e
 #Expiring in less than one month. We need to renew
 
 # Sign the request and obtain a certificate
